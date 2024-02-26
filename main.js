58a3a611f3fd7f1a4634b3ccb7f5862f25424e97
PatricioDVG <140539670+PatricioDVG@users.noreply.github.com>
2024-02-26 17:03:37 -0600

    Update main.js

--- main.js
+++ main.js
@@ -43,7 +43,7 @@ camera = document.getElementById("camera");
 function take_snapshot()
 {
     Webcam.snap(function(data_uri){
-        document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_uri+'"'
+        document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_uri+'">'
     }
     )
 }
